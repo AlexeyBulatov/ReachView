6750c9d3dcc5d77173ac6edaa77bf86a3699c4cd
Egor Fedorov <egor.fedorov@emlid.com>
2015-11-26 12:09:26 +0300

    updated GPS configs with airborne setting

--- rtklib_configs/reach_GPS_5Hz.cmd
+++ rtklib_configs/reach_GPS_5Hz.cmd
@@ -1,4 +1,4 @@
-# navigation data update rate
+# navigation data update rate 5Hz in ms
 !UBX CFG-RATE 200 1 1
 
 # turn on UBX RXM-RAWX messages on first(UART) interface with the same freq
@@ -30,14 +30,24 @@
 # set GLONASS 8-14 channels off
 !UBX CFG-GNSS 0 32 32 1 6 8 14 0 0 0 1 1
 
+# change NAV5 stationary mode to airborne <4g
+!UBX CFG-NAV5 1 8 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
 # turn off extra messages default messages
-!UBX CFG-MSG 240 0 0 0 0 0 0 0 # NMEA GGA
-!UBX CFG-MSG 240 1 0 0 0 0 0 0 # NMEA GLL
-!UBX CFG-MSG 240 2 0 0 0 0 0 0 # NMEA GSA
-!UBX CFG-MSG 240 3 0 0 0 0 0 0 # NMEA GSV
-!UBX CFG-MSG 240 4 0 0 0 0 0 0 # NMEA RMC
-!UBX CFG-MSG 240 5 0 0 0 0 0 0 # NMEA VTG
-!UBX CFG-MSG 240 8 0 0 0 0 0 0 # NMEA ZDA
+# NMEA GGA
+!UBX CFG-MSG 240 0 0 0 0 0 0 0
+# NMEA GLL
+!UBX CFG-MSG 240 1 0 0 0 0 0 0
+# NMEA GSA
+!UBX CFG-MSG 240 2 0 0 0 0 0 0
+# NMEA GSV
+!UBX CFG-MSG 240 3 0 0 0 0 0 0
+# NMEA RMC
+!UBX CFG-MSG 240 4 0 0 0 0 0 0
+# NMEA VTG
+!UBX CFG-MSG 240 5 0 0 0 0 0 0
+# NMEA ZDA
+!UBX CFG-MSG 240 8 0 0 0 0 0 0
 !UBX CFG-MSG 1 3 0 0 0 0 0 0
 !UBX CFG-MSG 1 3 0 0 0 0 0 0
 !UBX CFG-MSG 1 6 0 0 0 0 0 0
